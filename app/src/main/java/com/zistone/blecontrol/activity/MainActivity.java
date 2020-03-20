0563679e993186980e60bfffd132db81c17bce7e
QQ652276536 <652276536@qq.com>
2020-03-20 09:36:49 +0800

    百度TTS语音有几个库忘记提交了

--- app/src/main/java/com/zistone/blecontrol/activity/MainActivity.java
+++ app/src/main/java/com/zistone/blecontrol/activity/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.zistone.blecontrol.R;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     public static final int ACTIVITYRESULT_WRITEVALUE = 1;
     public static final int ACTIVITYRESULT_PARAMSETTING = 2;
     public static final int ACTIVITYRESULT_OTA = 3;
@@ -28,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
             _isPermissionRequested = true;
             ArrayList<String> permissionsList = new ArrayList<>();
             String[] permissions = {Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH, Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.MODIFY_AUDIO_SETTINGS,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.CHANGE_WIFI_STATE,
-                    Manifest.permission.CAMERA};
+                                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET,
+                                    Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_WIFI_STATE,
+                                    Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.CAMERA, Manifest.permission.WRITE_SETTINGS};
             for (String perm : permissions) {
                 if (PackageManager.PERMISSION_GRANTED != checkSelfPermission(perm)) {
                     //进入到这里代表没有权限
@@ -42,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), 1);
             }
         }
+    }
+
+    /**
+     * android 6.0以上动态授权的回调
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, ">>>android 6.0以上动态授权的回调");
     }
 
     @Override
